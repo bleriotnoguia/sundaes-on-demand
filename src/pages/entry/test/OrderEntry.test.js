b10ec3f62b478e80aa4f9a288bb72731a5822e60
Bleriot Noguia <nstevebleriot@yahoo.fr>
2022-12-29 12:42:09 +0100

    small bug fixed

--- src/pages/entry/test/OrderEntry.test.js
+++ src/pages/entry/test/OrderEntry.test.js
@@ -10,7 +10,7 @@ test("handles error for scoops and toppings routes", async () => {
     )
   );
   server.resetHandlers(
-    rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
       res(ctx.status(500))
     )
   );
